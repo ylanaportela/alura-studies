db7f656511da2eee1044af9f8a034313372aa54a
Ylana Portela <ylanaportela2000@gmail.com>
2022-05-24 16:28:58 -0300

    Creating form, list and button component and rendering in file App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Form from './components/Form';
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Form/>
+      <List/>
     </div>
   );
 }
