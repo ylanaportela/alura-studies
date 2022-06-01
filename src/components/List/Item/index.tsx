d56367ee17aa4c44326fdaae4b3780e87b44e870
Ylana Portela <ylanaportela2000@gmail.com>
2022-06-01 14:42:54 -0300

    add categories of itarefa

--- src/components/List/Item/index.tsx
+++ src/components/List/Item/index.tsx
@@ -1,9 +1,22 @@
 import { Itarefa } from '../../../types/ITarefa'
-import style from '../List.module.scss'
+import style from './Item.module.scss'
 
-export default function Item({tarefa, tempo, selecionado, completado, id}: Itarefa) {
+
+interface Props extends Itarefa{
+    selecionaTarefa: (tarefaSelecionada: Itarefa) => void
+}
+
+
+export default function Item({tarefa, tempo, selecionado, completado, id, selecionaTarefa}: Props) {
+
     return (
-        <li className={style.item}>
+        <li className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`} onClick={() => { selecionaTarefa({
+            tarefa,
+            tempo,
+            selecionado,
+            completado, 
+            id
+        })}}>
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
         </li>
