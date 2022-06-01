78ba8dad0bcf97fdcb3e709f7a694643ff8030fe
Ylana Portela <ylanaportela2000@gmail.com>
2022-06-01 02:52:07 -0300

    Add type for button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import style from './Button.module.scss'
-
-class Button extends React.Component <{children: any}>{
-    render(){
-        return (
-            <button className={style.botao}>
-                {this.props.children}
-            </button>
-        )
-    }
-}
-
+class Button extends React.Component<any, { type?:
+    "button" | "submit" | "reset" | undefined
+ }> {
+   render() {
+     const { type = "button" } = this.props;
+     return (
+       <button type={type} className={style.botao}>
+         {this.props.children}
+       </button>
+     )
+   }
+ }
 export default Button
+
+
+
+
+
 
 // export default function Button(props) {
 //     return (
