9a5c63233517cc51b9e16fd707675b3d52faf70b
Ylana Portela <ylanaportela2000@gmail.com>
2022-06-20 17:29:31 -0300

    Make the timer work for each task

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,28 +1,20 @@
-import React from "react";
-import style from './Button.module.scss'
-class Button extends React.Component<any, { type?:
-    "button" | "submit" | "reset" | undefined
- }> {
-   render() {
-     const { type = "button" } = this.props;
-     return (
-       <button type={type} className={style.botao}>
-         {this.props.children}
-       </button>
-     )
-   }
- }
-export default Button
+import React from 'react';
+import style from './Button.module.scss';
 
+class Button extends React.Component<{
+    children?: React.ReactNode
+    type?: "button" | "submit" | "reset" | undefined,
+    onClick?: () => void
+}> {
+    render() {
+        const { type = "button", onClick } = this.props;
+        return (
+            <button onClick={onClick} type={type} className=
+            {style.botao}>
+                {this.props.children}
+            </button>
+        )
+    }
+}
 
-
-
-
-
-// export default function Button(props) {
-//     return (
-//         <button className={style.botao}>
-//             {props.children}
-//         </button>
-//     )
-// }
+export default Button;
--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,16 +1,42 @@
+import { useEffect, useState } from "react";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { Itarefa } from "../../types/ITarefa";
 import Button from "../Button";
 import Clock from "./Clock";
 import style from "./Timer.module.scss"
 
-export default function Timer(){
+interface Props{
+    selecionado: Itarefa | undefined
+}
+
+
+export default function Timer({selecionado}: Props){
+    const [tempo, setTempo] = useState<number>()
+
+    useEffect(()=>{
+        if(selecionado?.tempo){
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
+    },[selecionado])
+
+
+    function regressiva(contador : number = 0){
+        setTimeout(()=>{
+            if(contador > 0){
+                setTempo(contador -1)
+                return regressiva(contador -1)
+            }
+        }, 1000)
+    }
+    
     return(
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetros</p>
-            <div className={style.relogioWrapper}> 
-                <Clock/>
+            <div className={style.relogioWrapper}>
+                <Clock tempo={tempo} />
             </div>
-            <Button>
-                Começar
+            <Button onClick={() => regressiva(tempo)}>
+                    Começar!
             </Button>
         </div>
     )
